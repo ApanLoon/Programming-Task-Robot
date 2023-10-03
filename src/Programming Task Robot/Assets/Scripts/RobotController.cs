9104341010bb21ab241a74c4a0d412f739d54084
ApanLoon <apanloon@live.co.uk>
2023-10-03 18:57:41 +0200

    Pre-traverse the path when creating the command.

--- src/Programming Task Robot/Assets/Scripts/RobotController.cs
+++ src/Programming Task Robot/Assets/Scripts/RobotController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RobotCommand
@@ -8,6 +9,8 @@ public class RobotCommand
     public Vector2Int MaxPos { get; protected set; }
 
     public Vector2Int StartPos { get; protected set; }
+
+    public List<Step> Steps {  get; protected set; }
 
     public enum Direction
     {
@@ -22,12 +25,23 @@ public class RobotCommand
         public Direction Direction;
         public int Distance;
 
+        public Vector2Int GetOffset()
+        {
+            return Direction switch
+            {
+                Direction.North => new Vector2Int(0, 1),
+                Direction.East => new Vector2Int(1, 0),
+                Direction.South => new Vector2Int(0, -1),
+                Direction.West => new Vector2Int(-1, 0),
+                _ => throw new NotImplementedException(),
+            };
+        }
+
         public override string ToString()
         {
             return $"{Direction}({Distance})";
         }
     }
-    public List<Step> Steps {  get; protected set; }
 
     // M:-10,10,-10,10;S:-5,5;[W5,E5,N4,E3,S2,W1]
 
@@ -83,7 +97,7 @@ public class RobotCommand
                             _ => throw new ArgumentOutOfRangeException(nameof(Direction), "Unexpected direction value")
                         };
                     }
-                    catch (ArgumentOutOfRangeException e)
+                    catch (ArgumentOutOfRangeException)
                     {
                         Debug.LogError ($"Invalid step format.");
                         return;
@@ -121,6 +135,32 @@ public class RobotCommand
         return dir;
     }
 
+    public bool TraverseSteps(out List<Vector2Int> path)
+    {
+        path = new List<Vector2Int>();
+        var currentPos = StartPos;
+        path.Add(currentPos);
+        foreach (var step in Steps)
+        {
+            for (var i = 0; i < step.Distance; i++)
+            {
+                currentPos += step.GetOffset();
+                if (IsInBounds(currentPos) == false)
+                {
+                    return false;
+                }
+                path.Add(currentPos);
+            }
+        }
+        return true;
+    }
+
+    public bool IsInBounds(Vector2Int currentPos)
+    {
+        return    currentPos.x >= MinPos.x && currentPos.x < MaxPos.x
+               && currentPos.y >= MinPos.y && currentPos.y < MaxPos.y;
+    }
+
     public override string ToString()
     {
         return $"MinPos={MinPos}, MaxPos={MaxPos}, StartPos={StartPos}, Steps={string.Join<Step>(", ", Steps)}";
@@ -132,6 +172,11 @@ public class RobotController : MonoBehaviour
     public float Speed = 2f;
     public float RotateDamping = 0.01f;
 
+    [SerializeField] protected GameObject TilePrefab;
+    [SerializeField] protected Transform TilesParent;
+    [SerializeField] protected Material DirtyMaterial;
+    [SerializeField] protected Material CleanMaterial;
+
     private RobotCommand _command;
     private Vector3 _velocity;
     private Vector3 _target;
@@ -142,7 +187,41 @@ public class RobotController : MonoBehaviour
         Debug.Log(command);
         _command = command;
 
+        SpawnDirtyTiles();
+
+        if (_command.TraverseSteps(out var path))
+        {
+            Debug.Log("Path will remain in bounds.");
+            Debug.Log("Unique positions that will be visited: " + string.Join(", ", path.Distinct()));
+        }
+        else
+        {
+            Debug.Log($"Path will leave bounds at {path[^1]}");
+            Debug.Log("Positions that will be visited: " + string.Join(", ", path));
+        }
+
         _target = new Vector3(_command.StartPos.x, 0f, _command.StartPos.y);
+    }
+
+    private readonly Dictionary<Vector2Int, GameObject> _tileMap = new();
+
+    private void SpawnDirtyTiles()
+    {
+        foreach (var tileGameObject in _tileMap.Values)
+        {
+            Destroy(tileGameObject);
+        }
+        _tileMap.Clear();
+
+        for (var x = _command.MinPos.x; x < _command.MaxPos.x; x++)
+        {
+            for (var y = _command.MinPos.y; y < _command.MaxPos.y; y++)
+            {
+                var tileGo = Instantiate(TilePrefab, new Vector3(x, 0f, y), Quaternion.identity, TilesParent);
+                tileGo.GetComponentInChildren<MeshRenderer>().material = DirtyMaterial;
+                _tileMap[new Vector2Int(x, y)] = tileGo;
+            }
+        }
     }
 
     public void Activate(string command)
@@ -161,7 +240,12 @@ public class RobotController : MonoBehaviour
         {
             transform.position = _target;
 
-            RobotCommand.Direction? dir = _command.GetNextDirection();
+            // Clean the tile:
+            _tileMap[new Vector2Int(Mathf.FloorToInt (transform.position.x), Mathf.FloorToInt(transform.position.z))]
+                .GetComponentInChildren<MeshRenderer>().material = CleanMaterial;
+
+            // Get a new target:
+            var dir = _command.GetNextDirection();
             if (dir == null)
             {
                 Debug.Log("Target reached");
@@ -169,7 +253,7 @@ public class RobotController : MonoBehaviour
                 return;
             }
 
-            Vector3 offset = dir switch
+            var offset = dir switch
             {
                 RobotCommand.Direction.North => new Vector3(0f, 0f, 1f),
                 RobotCommand.Direction.East => new Vector3(1f, 0f, 0f),
@@ -177,15 +261,16 @@ public class RobotController : MonoBehaviour
                 RobotCommand.Direction.West => new Vector3(-1f, 0f, 0f),
                 _ => throw new NotImplementedException(),
             };
+            _target = transform.position + offset;
+
             // Boundary check
             if (   _target.x < _command.MinPos.x || _target.x >= _command.MaxPos.x
                 || _target.y < _command.MinPos.y || _target.y >= _command.MaxPos.y)
             {
-                _target = offset = Vector3.zero;
+                _target = transform.position;
                 _command = null;
-                Debug.Log($"Unable to reach target. Target is outside the map. ({Mathf.FloorToInt(transform.position.x)}, {Mathf.FloorToInt(transform.position.y)})");
+                Debug.Log($"Unable to reach target. Target is outside the map. (currentPos={Mathf.FloorToInt(transform.position.x)}, {Mathf.FloorToInt(transform.position.z)})");
             }
-            _target = transform.position + offset;
 
             // TODO: Track path
             return;
